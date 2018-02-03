4aec05452d2141a067d42b7d5f83481294ed8fc4
idunin <ilya.mirea@gmail.com>
2018-02-04 00:26:05 +0300

    Article validations

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -14,4 +14,36 @@ RSpec.feature "Creating Article" do
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
   end
+
+  scenario "A user fails to create new article" do
+    visit "/"
+
+    click_link "New Article"
+
+    fill_in "Title", with: ""
+    fill_in "Body", with: ""
+
+    click_button "Create Article"
+
+    expect(page).to have_content("Arcticle has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+  end
+
+  scenario "A user fails to create new article with long title or body" do
+    TITLE_LENGTH = 255
+    BODY_LENGTH = 4096
+    visit "/"
+
+    click_link "New Article"
+
+    fill_in "Title", with: "A" * (TITLE_LENGTH.to_i + 1)
+    fill_in "Body", with: "A" * (BODY_LENGTH.to_i + 1)
+
+    click_button "Create Article"
+
+    expect(page).to have_content("Arcticle has not been created")
+    expect(page).to have_content("Title is too long (maximum is #{TITLE_LENGTH} characters)")
+    expect(page).to have_content("Body is too long (maximum is #{BODY_LENGTH} characters)")
+  end
 end
